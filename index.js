5a7eeb9d5f5a8cc7fc220db961376fb106b900b1
Kimonic <aimuaimarengiaye@gmail.com>
2022-12-21 17:24:35 -0500

    fixes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const createError = require('http-errors');
 
 const dotenv = require("dotenv");
 dotenv.config();
